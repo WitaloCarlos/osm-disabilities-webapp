26c8fb680f5e366f23466f87e05bccd82ee8f88d
Witalo Albuquerque <witalo.albuquerque@splab.ufcg.edu.br>
2017-03-07 15:00:46 -0300

    Tabela dos bairros

--- src/app/resume/resume.controller.js
+++ src/app/resume/resume.controller.js
@@ -15,15 +15,26 @@
 
         google.charts.setOnLoadCallback(drawTagCount);
 
+        google.charts.setOnLoadCallback(drawUsersCount);
+
         google.charts.setOnLoadCallback(drawTargetVAnnotationsCount);
 
         google.charts.setOnLoadCallback(drawGeneralVDisabilitiesCount);
+
+         google.charts.setOnLoadCallback(drawWheelchair);
 
         
 
 
 
         var self = $scope;
+
+        self.wheelchair_borough = null;
+
+         resumeService.getWheelchairByBorough(function (response) {
+                self.wheelchair_borough = response.data;
+            },
+                function (error) { console.log(error); })
 
         function drawTargets() {
             resumeService.getTargets(function (response) {
@@ -66,6 +77,27 @@
                 function (error) { console.log(error); })
         }
 
+        function drawUsersCount() {
+            resumeService.getUsersCount(function (response) {
+                var json = response.data;
+
+                var data = new google.visualization.arrayToDataTable(mountUsersCountArray(json));
+                var options = {
+                    title: "Quantidade de Voluntários por Tags de Acessibilidade",
+                    width: 650,
+                    height: 300,
+                    legend: { position: 'labeled' },
+                    pieSliceText: 'value'
+
+                }
+
+                var tree = new google.visualization.PieChart(document.getElementById('users_v_annotations'));
+
+                tree.draw(data, options);
+            },
+                function (error) { console.log(error); })
+        }
+
 
         function drawTargetVAnnotationsCount() {
             resumeService.getTags(function (response) {
@@ -77,7 +109,8 @@
                     width: 650,
                     height: 300,
                     legend: { position: 'labeled' },
-                    pieSliceText: 'value'
+                    pieSliceText: 'value',
+                    slices: {  2: {offset: 0.5}}
 
                 }
 
@@ -98,7 +131,8 @@
                     width: 650,
                     height: 300,
                     legend: { position: 'labeled' },
-                    pieSliceText: 'value'
+                    pieSliceText: 'value',
+                    slices: {  0: {offset: 0.5}}
 
                 }
 
@@ -109,8 +143,47 @@
                 function (error) { console.log(error); })
         }
 
+        function drawWheelchair() {
+            resumeService.getTagsCount(function (response) {
+                var json = response.data;
+
+                var data = new google.visualization.arrayToDataTable(mountWheelchairArray(json));
+                var options = {
+                    title: "Anotações de Acesso para Cadeirantes",
+                    width: 650,
+                    height: 300,
+                    legend: { position: 'labeled' },
+                    pieSliceText: 'value',
+                    slices: {  3: {offset: 0.5}}
+
+                }
+
+                var tree = new google.visualization.PieChart(document.getElementById('wheelchair_chart'));
+
+                tree.draw(data, options);
+            },
+                function (error) { console.log(error); })
+        }
+
 
         function mountTagsCountTreeArray(json) {
+
+            var array = [];
+            array[0] = ['Tag', 'Quantidade'];
+
+            for (var i = 0; i < json.length; i++) {
+                if (json[i]['id'] === 'all_tags') {
+                    // ignore
+                } else {
+                    array[i] = [json[i]['id'], json[i]['count']];
+                }
+            }
+
+
+            return array;
+        }
+
+         function mountUsersCountArray(json) {
 
             var array = [];
             array[0] = ['Tag', 'Quantidade'];
@@ -149,6 +222,53 @@
 
             return array;
         }
+
+        function mountWheelchairArray(json) {
+
+            var array = [];
+            array[0] = ['Valor', 'Quantidade'];
+
+            var yes = 0;
+            var no = 0;
+            var limited = 0;
+            var designated = 0;
+            var kerb_flush = 0;
+            var kerb_lowered = 0;
+            var desc = 0;
+
+            for (var i = 0; i < json.length; i++) {
+                if (json[i]['id'] === 'wheelchair_yes') {
+                    yes += json[i]['count'];
+                } else if (json[i]['id'] === 'wheelchair_no')  {
+                    no += json[i]['count'];
+                } else if (json[i]['id'] === 'wheelchair_limited')  {
+                    limited += json[i]['count'];
+                } else if (json[i]['id'] === 'wheelchair_designated')  {
+                    designated += json[i]['count'];
+                } else if (json[i]['id'] === 'kerb_flush')  {
+                    kerb_flush += json[i]['count'];
+                } else if (json[i]['id'] === 'kerb_lowered')  {
+                    kerb_lowered += json[i]['count'];
+                }  else if (json[i]['id'] === 'wheelchair_description')  {
+                    desc += json[i]['count'];
+                } else {
+                    // ignore
+                }
+            }
+
+            array[1] = ['Pleno', yes];
+            array[2] = ['Não', no];
+            array[3] = ['Limitado', limited];
+            array[4] = ['Designado', designated];
+            array[5] = ['Rampa baixa', kerb_lowered];
+            array[6] = ['Rampa nivelada', kerb_flush];
+            array[6] = ['Descrição específica', desc];
+
+
+
+            return array;
+        }
+
 
 
 
--- src/app/resume/resume.service.js
+++ src/app/resume/resume.service.js
@@ -23,6 +23,20 @@
                     .catch(errorCallback);
             },
 
+             getUsersCount: function (successCallback, errorCallback) {
+
+                return $http.get('../src/assets/json/user_tags_count.json')
+                    .then(successCallback)
+                    .catch(errorCallback);
+            },
+
+            getWheelchairByBorough: function (successCallback, errorCallback) {
+
+                return $http.get('../src/assets/json/wheelchair.json')
+                    .then(successCallback)
+                    .catch(errorCallback);
+            },
+
             getTags: function (successCallback, errorCallback) {
 
                 return $http.get('../src/assets/json/tags.json')
